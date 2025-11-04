f5e420ed7732644ed17e92305a72578ad4176379
kens-04 <okaken888@icloud.com>
2025-11-04 15:50:19 -0700

    expo test

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,45 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const typescriptEslint = require('@typescript-eslint/eslint-plugin');
+const typescriptParser = require('@typescript-eslint/parser');
 
 module.exports = defineConfig([
   expoConfig,
   {
-    ignores: ['dist/*'],
+    ignores: ['dist/*', 'node_modules/*', '.expo/*'],
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+    },
+    rules: {
+      // React/React Native推奨ルール
+      'react/react-in-jsx-scope': 'off', // React 17+ では不要
+      'react/prop-types': 'off', // TypeScript使用のため無効化
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      // 未使用インポート/変数の検出
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+      'no-unused-vars': 'off', // @typescript-eslint/no-unused-vars を使用
+      // Promise関連
+      '@typescript-eslint/no-floating-promises': 'error',
+    },
   },
 ]);
